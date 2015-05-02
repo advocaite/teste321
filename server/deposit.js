03a1f765502cebcd518e3d1c220eb3b8f36bbd8a
Martijn Buurman <hallo@m19.nl>
2015-05-02 10:51:50 +0200

    Send ok back

--- server/deposit.js
+++ server/deposit.js
@@ -1,7 +1,8 @@
 var database = require('./database');
 
-exports.callback = function () {
+exports.callback = function (req, res) {
   var transaction = req.body;
 
   console.log(transaction);
+  res.send('ok');
 };
