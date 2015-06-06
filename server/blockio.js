7c61a28704511007d110cdcb7164134551387539
Martijn Buurman <martijn@m19.nl>
2015-06-06 16:34:38 +0200

    Fix wrong parameters

--- server/blockio.js
+++ server/blockio.js
@@ -3,9 +3,9 @@ var BlockIo = require('block_io');
 var BLOCK_SECRET_KEY = process.env.BLOCK_SECRET_KEY;
 if (!BLOCK_SECRET_KEY) console.log('Must set BLOCK_SECRET_KEY');
 var BLOCK_API_KEY = process.env.BLOCK_BITCOIN_KEY;
-if (!BLOCK_SECRET_KEY) console.log('Must set BLOCK_BITCOIN_KEY');
+if (!BLOCK_API_KEY) console.log('Must set BLOCK_BITCOIN_KEY');
 
-var block_io = new BlockIo(BLOCK_SECRET_KEY, BLOCK_API_KEY, 2);
+var block_io = new BlockIo(BLOCK_API_KEY, BLOCK_SECRET_KEY, 2);
 
 module.exports = {
     createDepositAddress: function(userId, callback) {
