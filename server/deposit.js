4d4dbfb262b9817cecb38d2d480de29753dd0b6b
Martijn Buurman <martijn@m19.nl>
2015-06-17 11:38:39 +0200

    fix

--- server/deposit.js
+++ server/deposit.js
@@ -17,7 +17,7 @@ exports.callback = function(req, res) {
   }
 
   var body = req.body.data;
-  var amount = BigNumber(body.amount_received).times(Math.pow(10, 8)).toNumber();
+  var amount = new BigNumber(body.amount_received).times(Math.pow(10, 8)).toNumber();
 
   if (body.confirmations != 1 || Number(body.balance_change) < 0 || body.address === process.env.BLOCK_BITCOIN_WITHDRAWAL_ADDRESS) {
     console.log(body.confirmations);
